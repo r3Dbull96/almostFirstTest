77be9a8e898a3f3f3bb87bb85f0f1b6c1aa2371e
Georgy <r3dbull56@gmail.com>
2026-06-17 18:41:33 +0500

    Implemented POM for the homework test

--- src/test/java/ru/bulgakov/qa/AlmostFirstTests.java
+++ src/test/java/ru/bulgakov/qa/AlmostFirstTests.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import ru.bulgakov.pages.DemoqaPage;
+import ru.bulgakov.pages.PaymentPage;
 import ru.bulgakov.pages.YandexSearchPage;
 
 import static com.codeborne.selenide.Condition.text;
@@ -17,41 +19,36 @@ public class AlmostFirstTests {
         Configuration.pageLoadTimeout = 10000;
         Configuration.timeout = 10000;
 
-        open("https://ya.ru/", YandexSearchPage.class)
+        PaymentPage paymentPage = open("https://ya.ru/", YandexSearchPage.class)
                 .search("bulgakov qa")
                 .submit()
                 .closeDefaultBrowserSelectWindow()
                 .openLinkInNewTab("ivanbulgakovqa.ru")
                 .clickPrice()
                 .openGoToQaModal()
-                .choosePayment()
-                .getPrice().shouldHave(text("₽ 47 000.00"));
+                .choosePayment();
+
+        paymentPage.getPrice().shouldHave(text("₽ 47 000.00"));
     }
 
     @Test
+    @DisplayName("Проверить отображение введенных в форму Text Box значений")
+    @Tag("POSITIVE")
     void checkTextBoxValuesTest() {
-        /*
-         * Тест-кейс - проверить отображение введенных в форму Text Box значений
-         * 1. Перейти к форме Text Box
-         * 2. Заполнить все поля формы валидными значениями
-         * 3. Нажать кнопку Submit
-         * 4. Проверить что введенные на ш.2 значения отображаются на странице
-         * */
-
         Configuration.pageLoadTimeout = 10000;
         Configuration.timeout = 10000;
 
-        open("https://demoqa.com/text-box");
+        DemoqaPage demoqaPage = open("https://demoqa.com/text-box", DemoqaPage.class)
+                .setUsername("Иванов Иван Иванович")
+                .setEmail("test@mail.ru")
+                .setCurrentAddress("г. Москва, ул. Ленина, д. 1")
+                .setPermanentAddress("г. Москва, ул. Малышева, д. 1/2")
+                .scrollToSubmit()
+                .submit();
 
-        $("#userName").setValue("Иванов Иван Иванович");
-        $("#userEmail").setValue("test@mail.ru");
-        $("#currentAddress").setValue("г. Москва, ул. Ленина, д. 1");
-        $("#permanentAddress").setValue("г. Москва, ул. Малышева, д. 1/2");
-        $("#submit").scrollTo().click();
-
-        $("#name").shouldHave(text("Name:Иванов Иван Иванович"));
-        $("#email").shouldHave(text("Email:test@mail.ru"));
-        $("p#currentAddress").shouldHave(text("Current Address :г. Москва, ул. Ленина, д. 1"));
-        $("p#permanentAddress").shouldHave(text("Permananet Address :г. Москва, ул. Малышева, д. 1/2"));
+        demoqaPage.getDisplayedName().shouldHave(text("Name:Иванов Иван Иванович"));
+        demoqaPage.getDisplayedEmail().shouldHave(text("Email:test@mail.ru"));
+        demoqaPage.getDisplayedCurrentAddress().shouldHave(text("Current Address :г. Москва, ул. Ленина, д. 1"));
+        demoqaPage.getDisplayedPermanentAddress().shouldHave(text("Permananet Address :г. Москва, ул. Малышева, д. 1/2"));
     }
 }
