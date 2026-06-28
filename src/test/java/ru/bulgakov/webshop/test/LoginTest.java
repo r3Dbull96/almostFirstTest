e38164147507d09d9d7e2620f65da50d167d00cf
Georgy <r3dbull56@gmail.com>
2026-06-28 16:22:52 +0500

    Improvements from the lesson

--- src/test/java/ru/bulgakov/webshop/test/LoginTest.java
+++ src/test/java/ru/bulgakov/webshop/test/LoginTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.bulgakov.webshop.TestBase;
+import ru.bulgakov.webshop.pages.WsLoginPage;
 import ru.bulgakov.webshop.pages.WsRegistrationPage;
 import ru.bulgakov.webshop.pages.WsWelcomePage;
 
 import static com.codeborne.selenide.Selenide.*;
-import static ru.bulgakov.webshop.config.Config.WEB_SHOP_REGISTRATION_URL;
-import static ru.bulgakov.webshop.config.Config.WEB_SHOP_URL;
+import static ru.bulgakov.webshop.config.Config.*;
 
 public class LoginTest extends TestBase {
     private static final Faker faker = new Faker();
@@ -55,8 +55,7 @@ public class LoginTest extends TestBase {
     @Tag("negative")
     @CsvFileSource(resources = "/email.csv")
     void emailValidationErrorWhenLoggingTest(String email) {
-        open(WEB_SHOP_URL, WsWelcomePage.class)
-                .openLogin()
+        open(WEB_SHOP_LOGIN_URL, WsLoginPage.class)
                 .enterEmail(email)
                 .enterPassword(password)
                 .verifyEmailValidationErrorAppear()
